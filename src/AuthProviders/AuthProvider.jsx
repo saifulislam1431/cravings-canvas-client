0bd85dcf3ad25bbba269bf9e6c5f0a611b87632b
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-02 02:15:19 +0600

    Authentication setup

---
+++ src/AuthProviders/AuthProvider.jsx
@@ -0,0 +1,65 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+const auth = getAuth(app);
+const UserContext = createContext(null);
+
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const googleProvider = new GoogleAuthProvider();
+    const gitProvider = new GithubAuthProvider();
+
+    const newUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+    const googleSignIn = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const githubSignIn = () => {
+        return signInWithPopup(auth, gitProvider)
+    }
+
+    const resetPass = (email) => {
+        return sendPasswordResetEmail(auth, email)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+
+            setLoading(false);
+            setUser(currentUser);
+
+        })
+
+        return () => unsubscribe();
+    }, [])
+    const userInfo = {
+        user,
+        newUser,
+        signUser,
+        googleSignIn,
+        githubSignIn,
+        logOut,
+        resetPass
+    }
+
+    return (
+        <UserContext.Provider>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+export default AuthProvider;
